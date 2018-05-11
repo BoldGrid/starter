943fd1d4ed08425abac78693fdf048da861b58d6
Tim <dev@tim.ph>
2018-05-11 13:31:23 -0400

    initial commit of child theme for crio

--- functions.php
+++ functions.php
@@ -1,8 +1,51 @@
 <?php
 /**
- * This is where child-theme specific code should be ran.
+ * Crio Child functions.php
  *
- * @package Starter
+ * Adds functionality to theme.
+ *
+ * @since 1.0.0
  */
 
-?>
+/**
+ * Filters BGTFW Configurations.
+ *
+ * @param  array $config BGTFW configuration options.
+ *
+ * @return array $config BGTFW configuration options.
+ */
+function crio_child_config( $config ) {
+
+	// Override default provided palettes.
+	$config['customizer-options']['colors']['settings']['scss_directory']['default'] = get_stylesheet_directory();
+	$config['customizer-options']['colors']['defaults'] = array(
+		array(
+			'default' => true,
+			'format' => 'palette-primary',
+			'neutral-color' => '#000000',
+			'colors' => array(
+				'#000000',
+				'#000000',
+				'#000000',
+				'#000000',
+				'#000000',
+			),
+		),
+		array(
+			'format' => 'palette-primary',
+			'neutral-color' => '#ffffff',
+			'colors' => array(
+				'#ffffff',
+				'#ffffff',
+				'#ffffff',
+				'#ffffff',
+				'#ffffff',
+			),
+		),
+	);
+
+	return $config;
+}
+
+
+add_filter( 'boldgrid_theme_framework_config', 'crio_child_config', 11 );
